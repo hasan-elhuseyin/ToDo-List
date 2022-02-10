80725ba272afe404ca6f78fd01ea8687b3c26e78
Hasan-Elhussein <hasan.elhussein.97@gmail.com>
2022-02-10 03:18:38 +0300

    Add TaskCell

---
+++ To-Do List/To-Do List/Cells/TaskCell.swift
@@ -0,0 +1,27 @@
+//
+//  TaskCell.swift
+//  To-Do List
+//
+//  Created by Hasan Elhussein on 10.02.2022.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var completionDate: UILabel!
+    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+    
+}
