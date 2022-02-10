66967113b553d7bf455508f994e75bc988dac789
Hasan-Elhussein <hasan.elhussein.97@gmail.com>
2022-02-10 03:52:50 +0300

    Show mock data in tableview

--- To-Do List/To-Do List/Cells/TaskCell.swift
+++ To-Do List/To-Do List/Cells/TaskCell.swift
@@ -13,15 +13,15 @@ class TaskCell: UITableViewCell {
     @IBOutlet weak var detailLabel: UILabel!
     @IBOutlet weak var completionDate: UILabel!
     
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//        // Initialization code
-//    }
-//
-//    override func setSelected(_ selected: Bool, animated: Bool) {
-//        super.setSelected(selected, animated: animated)
-//
-//        // Configure the view for the selected state
-//    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
     
 }
